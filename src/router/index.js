643d8f2a33baca5cf52758bcf6b65bf0c8fcd3c7
leandrocodes <leandrovianacodes@gmail.com>
2021-02-27 23:07:05 -0400

    starting the admin client list page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../views/Login.vue'
+import store from '../store/index'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'Login',
     component: Login
+  },
+  {
+    path: '/admin/clients',
+    component: () => import('../views/admin/Clients.vue'),
+    meta: { adminAuth: true }
   }
 ]
 
@@ -16,6 +22,13 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, _from, next) => {
+  if (to.meta.adminAuth) {
+    store.state.isAdmin ? next() : next('/')
+  }
+  next()
 })
 
 export default router
