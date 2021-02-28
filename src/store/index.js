d8e350fa30cffbfe6fd13807eca6a32d1c149138
leandrocodes <leandrovianacodes@gmail.com>
2021-02-27 23:06:18 -0400

    using persisted store state

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
@@ -17,5 +18,6 @@ export default new Vuex.Store({
     }
   },
   actions: {},
-  modules: {}
+  modules: {},
+  plugins: [createPersistedState()]
 })
