b237bf96433e3d8692dfb6fa2a001cdd618a310c
leandrocodes <leandrovianacodes@gmail.com>
2021-02-28 17:37:00 -0400

    admin card improvements

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -10,12 +10,17 @@ export default {}
 
 <style lang="scss" scoped>
 .card {
-  max-width: 100%;
+  width: 100%;
+  max-width: 350px;
   min-height: 90px;
   max-height: 130px;
   background: $darker;
   border-radius: 7px;
   padding: 15px;
+  margin: 15px 0;
   box-shadow: rgba($color: #000000, $alpha: 0.15) 0 3px 6px;
+  &:last-of-type {
+    margin-bottom: 65px;
+  }
 }
 </style>
