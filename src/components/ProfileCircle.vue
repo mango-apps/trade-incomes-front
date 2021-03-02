c0a078949c729a35bd3807be10e6543371cebb3f
leandrocodes <leandrovianacodes@gmail.com>
2021-03-01 23:10:40 -0400

    profile circle component

---
+++ src/components/ProfileCircle.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="profile flex center flex-row vertical-center">
+    <h1 class="is-dark">{{ name | splitInitials }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true
+    }
+  },
+  filters: {
+    splitInitials(name) {
+      const splitted = name.split(' ')
+      return splitted[0].charAt(0) + splitted[1].charAt(0)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.profile {
+  width: 90px;
+  height: 90px;
+  background: $primary;
+  border-radius: 50%;
+}
+</style>
