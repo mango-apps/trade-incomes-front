f855daa9c4bf2ca50a2f27fb77b7f985f837cb7b
leandrocodes <leandrovianacodes@gmail.com>
2021-03-06 21:48:56 -0400

    importing vuex mutation

--- src/views/admin/Client.vue
+++ src/views/admin/Client.vue
@@ -48,46 +48,22 @@
     </div>
     <modal ref="add-funds">
       <template v-slot:header>
-        <h1>Modal title</h1>
+        <h1>Adicionar Fundos</h1>
       </template>
 
-      <template v-slot:body>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc sed
-          velit dignissim sodales ut eu sem integer vitae. Id aliquet lectus
-          proin nibh nisl condimentum. Fringilla urna porttitor rhoncus dolor
-          purus. Nam aliquam sem et tortor. Nisl vel pretium lectus quam id.
-          Cras pulvinar mattis nunc sed. Quis ipsum suspendisse ultrices gravida
-          dictum fusce ut placerat orci. Tristique magna sit amet purus.
-          Fermentum et sollicitudin ac orci phasellus egestas tellus. Erat
-          pellentesque adipiscing commodo elit at imperdiet dui accumsan. Felis
-          eget nunc lobortis mattis aliquam faucibus. Tincidunt eget nullam non
-          nisi est sit amet facilisis. Mi in nulla posuere sollicitudin aliquam
-          ultrices sagittis orci. Vitae proin sagittis nisl rhoncus mattis
-          rhoncus urna neque. Eget nunc scelerisque viverra mauris in aliquam
-          sem fringilla ut. Nec nam aliquam sem et tortor consequat id. Commodo
-          nulla facilisi nullam vehicula ipsum a. Elementum tempus egestas sed
-          sed. Faucibus purus in massa tempor nec feugiat nisl pretium fusce.
-        </p>
-        <p>
-          Arcu cursus vitae congue mauris rhoncus aenean. Tempor id eu nisl nunc
-          mi. Pharetra diam sit amet nisl suscipit adipiscing bibendum. Ut
-          faucibus pulvinar elementum integer enim. Odio facilisis mauris sit
-          amet massa vitae tortor condimentum lacinia. Eu non diam phasellus
-          vestibulum lorem sed risus. Porttitor rhoncus dolor purus non enim
-          praesent. Sit amet mauris commodo quis imperdiet. Lobortis feugiat
-          vivamus at augue eget. Nibh tellus molestie nunc non blandit. Tellus
-          mauris a diam maecenas sed enim ut. Tortor aliquam nulla facilisi cras
-          fermentum odio eu feugiat pretium. Mattis aliquam faucibus purus in
-          massa.
-        </p>
-      </template>
+      <template v-slot:body> </template>
 
       <template v-slot:footer>
         <div>
-          <button @click="$refs.modalName.closeModal()">Cancel</button>
-          <button @click="$refs.modalName.closeModal()">Save</button>
+          <button
+            class="button button__ghost button__danger"
+            @click="setAddFundsModal(false)"
+          >
+            Cancelar
+          </button>
+          <button class="button button__primary" @click="() => {}">
+            Salvar
+          </button>
         </div>
       </template>
     </modal>
@@ -98,6 +74,7 @@
 import ProfileCircle from '@/components/ProfileCircle.vue'
 import Card from '@/components/Card.vue'
 import Modal from '@/components/Modal.vue'
+import { mapMutations } from 'vuex'
 export default {
   name: 'ClientDetails',
   components: { Card, ProfileCircle, Modal },
@@ -123,6 +100,9 @@ export default {
     } finally {
       this.loading = false
     }
+  },
+  methods: {
+    ...mapMutations(['setAddFundsModal'])
   }
 }
 </script>
@@ -205,6 +185,12 @@ export default {
     to {
       padding-left: 25px;
     }
+  }
+}
+
+/deep/.modal {
+  .button.button__ghost.button__danger {
+    margin-right: 10px;
   }
 }
 </style>
