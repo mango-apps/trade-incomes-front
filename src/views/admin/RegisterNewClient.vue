f09da1b0fd22a81e8ec0d9b8ad081a7cf6456238
leandrocodes <leandrovianacodes@gmail.com>
2021-03-08 22:06:37 -0300

    registering user from admin panel

--- src/views/admin/RegisterNewClient.vue
+++ src/views/admin/RegisterNewClient.vue
@@ -11,9 +11,12 @@
         type="email"
         name="email"
         id="email"
-        class="input"
+        :class="['input', { dirty: emailDirty }]"
         placeholder="joao@email.com"
+        v-model="email"
+        required
       />
+      <span class="is-red" v-if="emailDirty">{{ emailDirty }}</span>
 
       <label for="nome" class="label">Nome:</label>
       <input
@@ -22,6 +25,8 @@
         id="nome"
         class="input"
         placeholder="João Silva"
+        v-model="name"
+        required
       />
 
       <label for="telefone" class="label">Telefone:</label>
@@ -33,6 +38,7 @@
         v-mask="'(##) #####-####'"
         v-model="phone"
         placeholder="(67) 99123-4567"
+        required
       />
 
       <label for="cpf" class="label">CPF:</label>
@@ -40,10 +46,13 @@
         type="text"
         name="cpf"
         id="cpf"
-        class="input"
+        :class="['input', { dirty: cpfDirty }]"
         v-mask="'###.###.###-##'"
+        v-model="cpf"
         placeholder="123.456.789-10"
+        required
       />
+      <span class="is-red" v-if="cpfDirty">{{ cpfDirty }}</span>
 
       <button type="submit">
         Finalizar
@@ -55,18 +64,55 @@
 <script>
 export default {
   data: () => ({
-    phone: null
-  })
+    name: null,
+    email: null,
+    cpf: null,
+    phone: null,
+    emailDirty: null,
+    cpfDirty: null
+  }),
+  watch: {
+    email() {
+      this.emailDirty = null
+    }
+  },
+  methods: {
+    async register() {
+      const payload = {
+        name: this.name,
+        email: this.email,
+        cpf: this.cpf,
+        phone: this.phone,
+        password: 'senha123'
+      }
+
+      try {
+        const { data } = await this.$axios.post('/auth/register', {
+          ...payload
+        })
+        if (data.email) {
+          this.$router.push({ name: 'Client List' })
+        }
+      } catch ({ response }) {
+        if (response.data.error === 'Email already registered') {
+          this.emailDirty = 'Email já registrado anteriormente'
+        }
+        if (response.data.error === 'CPF already registered') {
+          this.cpfDirty = 'CPF já registrado anteriormente'
+        }
+      }
+    }
+  }
 }
 </script>
 <style lang="scss" scoped>
 .form {
   .label {
-    margin: 5px 0;
+    margin-top: 12px;
   }
   button {
     height: 2.25rem;
-    margin-top: 2rem;
+    margin-top: 3rem;
     width: 100%;
     border: transparent;
     border-radius: 7px;
