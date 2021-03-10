cf9f7b81891513b8e8bf9e9345281f9e0e9c96d1
leandrocodes <leandrovianacodes@gmail.com>
2021-03-09 20:05:40 -0400

    userview router-view

---
+++ src/templates/UserView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="user-view">
+    <user-nav />
+    <router-view />
+  </div>
+</template>
+
+<script>
+import UserNav from '../components/UserNav.vue'
+export default {
+  components: { UserNav }
+}
+</script>
