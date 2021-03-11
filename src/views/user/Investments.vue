b0b4212add328042787b57553cce2c8df1df8cb3
leandrocodes <leandrovianacodes@gmail.com>
2021-03-10 22:03:30 -0400

    Step Modals - Withdrawal request

--- src/views/user/Investments.vue
+++ src/views/user/Investments.vue
@@ -3,7 +3,7 @@
     <h1 class="title">Meus investimentos</h1>
     <div class="card-list flex flex-column">
       <Card v-for="fund in funds" :key="fund._id">
-        <div class="fund-details" @click="openFund(fund)">
+        <div class="fund-details" @click="openModal(fund)">
           <div>
             <p class="bold fund-title">
               Fundo:
@@ -41,16 +41,117 @@
         </div>
       </Card>
     </div>
+    <modal v-if="step === 1">
+      <template v-slot:header>
+        <h1 class="title fs-medium">Sacar Investimento</h1>
+      </template>
+      <template v-slot:body>
+        <div class="balance">
+          <h3 class="balance__title is-bold txt-center">Saldo</h3>
+          <p v-if="balance" class="balance__value is-green txt-center">
+            {{
+              balance.toLocaleString('pt-BR', {
+                style: 'currency',
+                currency: 'BRL'
+              })
+            }}
+          </p>
+        </div>
+
+        <label class="label" for="gain">Valor do saque:</label>
+        <input
+          id="gain"
+          type="number"
+          class="input"
+          placeholder="R$ 1.000,00"
+          v-model="widthdrawValue"
+          :class="{ dirty: balanceDirty }"
+        />
+        <h6 class="balance__warn">
+          O seu depósito será realizado em até 2 dias úteis.
+        </h6>
+      </template>
+      <template v-slot:footer>
+        <div>
+          <button
+            class="button button__ghost button__danger"
+            @click="closeModal"
+          >
+            Cancelar
+          </button>
+          <button class="button button__primary" @click="setWithdrawValue">
+            Continuar
+          </button>
+        </div>
+      </template>
+    </modal>
+    <modal v-if="step === 2">
+      <template v-slot:header>
+        <h1 class="title fs-medium">Como deseja receber?</h1>
+      </template>
+      <template v-slot:body>
+        <label class="label" for="gain">Valor do saque:</label>
+      </template>
+      <template v-slot:footer>
+        <div>
+          <button
+            class="button button__ghost button__danger"
+            @click="closeModal"
+          >
+            Cancelar
+          </button>
+          <button class="button button__primary">
+            Salvar
+          </button>
+        </div>
+      </template>
+    </modal>
   </div>
 </template>
 
 <script>
 import Card from '@/components/Card.vue'
+import Modal from '@/components/Modal.vue'
+import { mapMutations } from 'vuex'
+
 export default {
-  components: { Card },
+  components: { Card, Modal },
+
   data: () => ({
-    funds: []
+    funds: [],
+    balance: 0,
+    widthdrawValue: null,
+    balanceDirty: null,
+    step: 1
   }),
+
+  methods: {
+    ...mapMutations(['setAddFundsModal']),
+    openModal(fund) {
+      this.balance = fund.gained
+      this.setAddFundsModal(true)
+    },
+
+    closeModal() {
+      this.setAddFundsModal(false)
+      this.balance = null
+      this.widthdrawValue = null
+      this.step = 1
+    },
+
+    setWithdrawValue() {
+      if (this.widthdrawValue > this.balance) {
+        this.balanceDirty = 'Não é possível sacar essa quantia.'
+      } else {
+        this.setAddFundsModal(false)
+        this.step++
+        setTimeout(() => {
+          this.setAddFundsModal(true)
+        }, 300)
+      }
+    }
+  },
+
   async created() {
     try {
       const { data } = await this.$axios.get('/user/funds')
@@ -89,6 +190,25 @@ export default {
         margin: 0;
       }
     }
+  }
+}
+
+.modal {
+  .button.button__ghost.button__danger {
+    margin-right: 10px;
+  }
+  .balance__title {
+    font-weight: 400;
+    margin-bottom: 0;
+    padding-bottom: 0;
+  }
+  .balance__value {
+    margin-top: 0.25rem;
+  }
+  .balance__warn {
+    margin-top: 5px;
+    font-weight: normal;
+    font-size: 12px;
   }
 }
 </style>
