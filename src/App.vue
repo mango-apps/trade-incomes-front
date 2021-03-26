467fd4365fdb5607ac9d3ad993d7ca7b0789cda4
leandrocodes <leandrovianacodes@gmail.com>
2021-03-25 21:56:37 -0400

    global toasty css

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,21 @@
   </div>
 </template>
 
+<style lang="scss">
+.toasty {
+  display: flex !important;
+  justify-content: left !important;
+  font-family: 'Quicksand', sans-serif;
+  font-weight: bold !important;
+  text-align: center !important;
+  background-color: $danger !important;
+
+  svg {
+    margin-right: 15px;
+  }
+}
+</style>
+
 <script>
 export default {
   name: 'App',
