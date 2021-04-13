1adfeb3c1259069e5e1926ce69d2d078adac36f6
leandrocodes <leandrovianacodes@gmail.com>
2021-04-13 19:17:43 -0400

    admin logout

---
+++ src/views/admin/Settings.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container">
+    <h1 class="title">Configurações</h1>
+    <button>Trocar senha <uil-key-skeleton size="30px" /></button>
+    <button @click.prevent="logout">Sair <uil-signout size="30px" /></button>
+  </div>
+</template>
+
+<script>
+import { UilSignout, UilKeySkeleton } from '@iconscout/vue-unicons'
+import { mapMutations } from 'vuex'
+
+export default {
+  components: { UilSignout, UilKeySkeleton },
+  methods: {
+    ...mapMutations(['setToken', 'setIsAdmin']),
+    logout() {
+      this.setToken('')
+      this.setIsAdmin(false)
+
+      localStorage.removeItem('vuex')
+
+      this.$router.push('/')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+button {
+  height: 3.25rem;
+  margin-top: 2rem;
+  width: 100%;
+  border: transparent;
+  border-radius: 7px;
+  font-weight: bold;
+  color: $background;
+  font-size: 1.15rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  &:nth-of-type(2) {
+    color: $foreground;
+    background: $danger;
+  }
+  svg {
+    margin-left: 5px;
+  }
+}
+</style>
